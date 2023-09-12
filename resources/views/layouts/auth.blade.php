8c07e3685c818c569c628a0324210e830daa4faa
Daniel Felix <felixdaniel2056@gmail.com>
2023-09-12 17:38:56 +0100

    chore: added desc

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -3,11 +3,11 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
-    <meta name="description" content="Smarthr - Bootstrap Admin Template">
+    <meta name="description" content="If you are looking for a financial institution that is committed to helping you reach your financial goals, Famous Co-operative is the right choice for you. We invite you to join us today and start building a brighter financial future!">
     <meta name="keywords" content="admin, estimates, bootstrap, business, corporate, creative, management, minimal, modern, accounts, invoice, html5, responsive, CRM, Projects">
     <meta name="author" content="Dreamguys - Bootstrap Admin Template">
     <meta name="robots" content="noindex, nofollow">
-    <title>Dashboard - HRMS admin template</title>
+    <title>Authentication - Famous Co-operative</title>
 
     <link rel="shortcut icon" type="image/x-icon" href="{{ asset('images/favicon.png') }}">
 
